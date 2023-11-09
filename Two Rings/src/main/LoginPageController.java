342873368d46e17af27f1bac68e3454c9ad215d1
TKatana <turjohalder41@gmail.com>
2023-11-09 20:56:53 +0600

    login success massage

--- Two Rings/src/main/LoginPageController.java
+++ Two Rings/src/main/LoginPageController.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package main;
 
@@ -10,25 +10,33 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
+ * FXML Controller class
  *
  * @author acer
  */
 public class LoginPageController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Label ShowInLabel;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TextField userNameTextField;
+    @FXML
+    private TextField userIdTextfield;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void logInbutton(ActionEvent event) {
+        ShowInLabel.setText("yes, Login is Succesful!!");
+    }
     
 }
