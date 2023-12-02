de904e66277e55f8672498df10405e7ae0b02fff
Fahim <2111476@iub.edu.bd>
2023-12-02 17:04:47 +0600

    ddd

--- Two Rings/src/main/CostumesLawyerSceneController.java
+++ Two Rings/src/main/CostumesLawyerSceneController.java
@@ -4,12 +4,19 @@
  */
 package main;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -17,6 +24,15 @@ import javafx.fxml.Initializable;
  * @author LENOVO ThinkPad
  */
 public class CostumesLawyerSceneController implements Initializable {
+
+    @FXML
+    private TextField userTextField;
+    @FXML
+    private TextField eventLocationTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private DatePicker eventdatePik;
 
     /**
      * Initializes the controller class.
@@ -29,6 +45,52 @@ public class CostumesLawyerSceneController implements Initializable {
     @FXML
     private void backbuttononclick(ActionEvent event)  throws IOException{
            SceneSwitcher switchTolawyerScene = new SceneSwitcher("CostumerDashBoardScene.fxml", event);
+        switchTolawyerScene.ConfirmSceneSwitch();
+    }
+
+    @FXML
+    private void sentButtonOnMouseClick(ActionEvent event) {
+        Customerlawyertable i = new Customerlawyertable(
+                userTextField.getText(), 
+                emailTextField.getText(),
+                eventdatePik.getValue(),
+                eventLocationTextField.getText());
+        
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        File f = null;
+        try {
+            f = new File("Customer Lawyer Info.bin");
+            if (f.exists()) {
+                fos = new FileOutputStream(f, true);
+                oos = new AppendableObjectOutputStream(fos);
+            } else {
+                fos = new FileOutputStream(f);
+                oos = new ObjectOutputStream(fos);
+            }
+
+            oos.writeObject(i);
+            System.out.println("write object sucessfull ");
+
+        } catch (IOException ex) {
+            Logger.getLogger(CustomerLawyerModel.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(CustomerLawyerModel.class
+                        .getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @FXML
+    private void signOutOnClick(ActionEvent event) throws IOException{
+           SceneSwitcher switchTolawyerScene = new SceneSwitcher("SignInScene.fxml", event);
         switchTolawyerScene.ConfirmSceneSwitch();
     }
     
--- Two Rings/src/main/Lawyer.java
+++ Two Rings/src/main/Lawyer.java
@@ -15,10 +15,14 @@ public class Lawyer extends Employee{
     private int serviceCharge;
     private List<Object> customers;
 
+<<<<<<< Updated upstream
 //    public Lawyer(int serviceCharge, List<Object> customers) {
 //        this.serviceCharge = serviceCharge;
 //        this.customers = customers;
 //    }
+=======
+   
+>>>>>>> Stashed changes
 
     public Lawyer(int serviceCharge, List<Object> customers, int employeeID, LocalDate joinDate, int salary, String designation, String name, String gender, String email, String phoneNo, String password, LocalDate dateOfBirth) {
         super(employeeID, joinDate, salary, designation, name, gender, email, phoneNo, password, dateOfBirth);
